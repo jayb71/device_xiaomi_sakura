8bb53689b96f47863bbd068eb5225d3014638443
Jay Bharambe <112482222+jayb71@users.noreply.github.com>
2023-10-17 17:57:01 +0000

    sakura: Adapt BoardConfig for panda kernel with proton clang as default clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,8 +36,11 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
-TARGET_KERNEL_CONFIG := sakura_defconfig
+TARGET_KERNEL_CONFIG := daisy_defconfig
 TARGET_KERNEL_VERSION := 4.9
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
+KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin/aarch64-linux-gnu-
 
 # Kernel additional flags
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
