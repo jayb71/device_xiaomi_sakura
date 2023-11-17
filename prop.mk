72880fbe77ddf3bf46fd49cbc0cdaf6e67048516
basamaryan <basam.aryan@gmail.com>
2023-11-17 18:40:44 +0530

    sakukra: Switch to full IWLAN mode
    [basamaryan: This is needed for Android U to fix RIL]

--- prop.mk
+++ prop.mk
@@ -219,7 +219,6 @@ persist.data.iwlan.ipsec.ap=1 \
 ril.subscription.types=NV,RUIM \
 rild.libargs=-d/dev/smd0 \
 rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-ro.telephony.iwlan_operation_mode=legacy \
 ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,20 \
 persist.sys.fflag.override.settings_network_and_internet_v2=true \
